c65e746e308ee454eb91e378e41ece1e414c1802
dangkhoa <dangkhoa123bn@gmail.com>
2025-01-26 16:07:58 +0700

    Initial Commit

---
+++ WheelSpinApp/WheelSpinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelSpinAppApp.swift
+//  WheelSpinApp
+//
+//  Created by Dang Khoa Dang Le on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WheelSpinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
