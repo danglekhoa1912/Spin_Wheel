90c7ec733d8d85efee9c8b432c1005b7f380c9f0
dangkhoa <dangkhoa123bn@gmail.com>
2025-01-29 18:19:37 +0700

    UI for create spin wheel

---
+++ WheelSpinApp/Model/SpinWheelModel.swift
@@ -0,0 +1,32 @@
+//
+//  SpinWheelModel.swift
+//  WheelSpinApp
+//
+//  Created by Dang Khoa Dang Le on 28/01/2025.
+//
+
+import Foundation
+
+struct Label: Identifiable {
+    let id = UUID()
+    let value: String
+}
+
+class SpinWheelModel: ObservableObject {
+    @Published var labels: [Label] = []
+    @Published var title: String = ""
+    @Published var newLabel: String = ""
+    
+    var isValid: Bool {
+        return !title.isEmpty && !labels.isEmpty
+    }
+    
+    func addNewLabel() {
+        labels.append(Label(value: newLabel))
+        newLabel = ""
+    }
+    
+    func deleteLabel(at offset: IndexSet) {
+        labels.remove(atOffsets: offset)
+    }
+}
